b4bd4a26235c12c6840527a4c206b0a5bd59183e
tech Influences <139861226+techinfluences@users.noreply.github.com>
2023-08-14 19:56:22 +0530

    Add files via upload

---
+++ Login/firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC4Vv5QCJR6l5hT6mQPGB_2v3DIDSn55io",
+    authDomain: "tech-influences-30f7b.firebaseapp.com",
+    databaseURL: "https://tech-influences-30f7b-default-rtdb.firebaseio.com",
+    projectId: "tech-influences-30f7b",
+    storageBucket: "tech-influences-30f7b.appspot.com",
+    messagingSenderId: "491613117899",
+    appId: "1:491613117899:web:84888a534073960ec22a4e",
+    measurementId: "G-QXPN4XHH97"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
