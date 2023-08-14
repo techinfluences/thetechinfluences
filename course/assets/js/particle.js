f8355c2e365264e887825fb5b57501fdb88758e8
tech Influences <139861226+techinfluences@users.noreply.github.com>
2023-08-14 19:46:34 +0530

    Delete course directory

--- course/assets/js/particle.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    Particles.init({
-      selector: ".background",
-      color: "#99cd32",
-      // connectParticles: true,
-      sizeVariations: 5,
-      maxParticles: 150,
-    });
-  };
