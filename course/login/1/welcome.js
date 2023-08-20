6094330c5c3b789812740495586db1d683f84b87
tech Influences <139861226+techinfluences@users.noreply.github.com>
2023-08-20 16:55:58 +0530

    update

--- course/login/1/welcome.js
+++ course/login/1/welcome.js
@@ -2,7 +2,7 @@ firebase.auth().onAuthStateChanged((user)=>{
     if(!user){
         location.replace("index.html")
     }else{
-        document.getElementById("user").innerHTML = "Thanks for login, "+user.email
+        document.getElementById("user").innerHTML = "You have already logged in, "+user.email
     }
 })
 
